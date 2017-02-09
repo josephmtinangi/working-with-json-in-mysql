ca345ee69c10dd15821cf46042a03f2dbd06419b
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-09 10:52:52 +0300

    Add fillables

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'attributes'];
     protected $cast = [
     	'attributes' => 'array'
     ];
